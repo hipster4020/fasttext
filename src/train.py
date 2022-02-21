23cb1877831309cacfabfc5b9dd11f8e0f0019a1
hipster4020 <hipster4020@gmail.com>
2022-02-21 19:44:35 +0900

    train 수정

--- src/train.py
+++ src/train.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
-import re
-import pandas as pd
-import pymysql
 import pickle
-from sqlalchemy import create_engine
-import modin.pandas as mp
 
-from gensim.models import Word2Vec, FastText
-from konlpy.tag import Okt
-import matplotlib.pyplot as plt
+import pandas as pd
+from gensim.models import FastText
 from gensim.test.utils import get_tmpfile
 
 import config
@@ -17,21 +11,28 @@ import config
 # data load
 def data_load():
     # pickle load
-    with open(config.data_dir, 'rb') as f:
+    with open(config.data_dir, "rb") as f:
         df = pickle.load(f)
-    df = mp.DataFrame(df)
-    
-    df = df[df.astype(str)['token'] != '[]']
+    df = pd.DataFrame(df)
+
+    df = df[df.astype(str)["token"] != "[]"]
     tokenized_data = df.token.tolist()
-    
+
     return tokenized_data
 
 
 # fasttext
 def fasttext(tokenized_data):
-    model = FastText(sentences = tokenized_data, window = config.window, min_count = config.min_count, workers = config.workers, sg = config.sg)
-    
+    model = FastText(
+        sentences=tokenized_data,
+        window=config.window,
+        min_count=config.min_count,
+        workers=config.workers,
+        sg=config.sg,
+    )
+
     return model
+
 
 if __name__ == "__main__":
     tokenized_data = data_load()
@@ -40,8 +41,8 @@ if __name__ == "__main__":
     # train & save
     model = fasttext(tokenized_data)
     fname = get_tmpfile(config.fname_dir)
-    fasttext.save(fname)
-    
+    model.save(fname)
+
     # similar test
     sim_test = model.wv.most_similar("엘지")
     print(sim_test)
